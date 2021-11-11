3540dcdbcf906e06c6285bda38183d61c64d7e70
chiashenglin <bryan2160088@gmail.com>
2021-11-11 14:34:54 -0800

    learn ios

--- What-The-Fridge-iOS/ViewController.swift
+++ What-The-Fridge-iOS/ViewController.swift
@@ -15,23 +15,56 @@ class ViewController: UIViewController {
         imageView.backgroundColor = .white
         return imageView
     }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Random Photo", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBlue
         view.addSubview(imageView)
         imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         imageView.center = view.center
         getRandomPhoto()
+        view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(didTapButton),
+                         for: .touchUpInside)
+    }
+    
+    @objc func didTapButton(){
+        getRandomPhoto()
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        button.frame = CGRect(x: 20,
+                              y: view.frame.size.height-50-view.safeAreaInsets.bottom,
+                              width: view.frame.size.width-40,
+                              height: 50)
+        
+    }
+    
     func getRandomPhoto() {
-        let urlString = "https://source.unsplash.com/random/600x600"
-        let url = URL(string: urlString)!
-        guard let data = try? Data(contentsOf: url) else {
-            return
+        let urlString = URL(string: "https://dog.ceo/api/breeds/image/random")!
+        var request = URLRequest(url: urlString)
+        request.httpMethod = "GET"
+        
+        
+        
+        var data1 = Data()
+        let _ = URLSession.shared.dataTask(with: request) {(data, response, error) in
+            guard let data = data else { return }
+            data1 = data
+            print("Data is: \(data1)!")
         }
-        imageView.image = UIImage(data: data)
+        imageView.image = UIImage(data: data1)
     }
 }
 
